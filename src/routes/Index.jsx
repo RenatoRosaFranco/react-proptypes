619437883c479291032d2ea4f344a37bec0c7127
Renato Rosa Franco <renato_ny@live.com>
2023-10-04 11:55:00 -0300

    feature: improving routes index file.

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import HomePage from '../pages/Todos/Index';
-import TodosPage from ''
+import TodosPage from '../pages/Todos/Index';
+import LoginPage from "../pages/Login/Index";
 
 const RoutesApp = () => {
 	return(
 			<Routes>
 				<Route path='/' element={ <HomePage /> } />
 				<Route path='/todos' element={ <TodosPage /> } />
+				<Route path='/login' element={ <LoginPage /> } />
 
 				<Route path='*' element={ <HomePage /> } />
 			</Routes>
