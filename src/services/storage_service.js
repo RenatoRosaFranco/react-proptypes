11de6771b83c510f931b8684917c0a9363735be4
Renato Rosa Franco <renato_ny@live.com>
2023-10-04 10:04:57 -0300

    feature: updating storage service in the project.

--- src/services/storage_service.js
+++ src/services/storage_service.js
@@ -1,5 +1,23 @@
 class StorageService {
 	constructor() {
+		this.stoge = localStorage;
+	}
+
+	getItem(item) {
+		try {
+			return this.localStorage.getItem(item) || null;
+		} catch (error) {
+			console.log('Error getting item from LocalStorage:', error);
+			return null;
+		}
+	}
+
+	setItem(item, value) {
+		try {
+			this.localStorage.setItem(item, value);
+		} catch (error) {
+			console.log('Error setting item in LocalStorage:', error);
+		}
 	}
 }
 
