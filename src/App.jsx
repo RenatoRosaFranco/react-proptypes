a38ed9debfe5e71150063c13cad875d94097b5fb
Renato Rosa Franco <renato_ny@live.com>
2023-10-03 23:29:58 -0300

    feature: propTypes package added and todos const created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
+  const todos = [
+    {id: 1, text: 'Task 1', completed: false},
+    {id: 2, text: 'Task 2', completed: true}
+  ]
 
+  return (
+    <div>
+      <TodoList todos={todos} />
     </div>
   );
 }
