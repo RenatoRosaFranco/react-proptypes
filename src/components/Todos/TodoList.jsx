3a610bd88f102d302c2f44848a5c92b1511f3893
Renato Rosa Franco <renato_ny@live.com>
2023-10-03 23:53:32 -0300

    feature: TodoItem component added.

--- src/components/Todos/TodoList.jsx
+++ src/components/Todos/TodoList.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
+import PropTypes from 'prop-types';
+import TodoItem from "./TodoItem";
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
+  const [todoList, setTodoList] = useState(todos);
+  const nextId = useRef(todos.length + 1);
+
+  const addTodo = (text) => {
+    const newTodo = {
+      id: nextId.current++,
+      text,
+      completed: false
+    }
+
+    setTodoList([...todoList, newTodo])
+  };
+
+  const deleteTodo = (id) => {
+    setTodoList(todos.filter((todo) => todo.id !== id));
+  };
+
+  const toggleTodo = (id) => {
+    setTodoList(
+      todoList.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    )
+  };
+
   return(
-    <>
-      <h1>Todo List</h1>
-    </>
+    <div>
+      <h2>Todo List</h2>
+      <button onClick={() => addTodo('New Task')}></button>
+      <br/>
+
+      {todoList.map((todo) => {
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          onDelete={deleteTodo}
+          onToggle={toggleTodo}
+        />
+      })}
+    </div>
   )
 }
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      completed: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
+};
 
 export default TodoList;
