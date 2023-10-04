87281a2c016fb622320d0c17fec48e187fd35bfc
Renato Rosa Franco <renato_ny@live.com>
2023-10-04 13:26:08 -0300

    feature: adding protected routes.

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -5,16 +5,18 @@ import HomePage from '../pages/Home/Index';
 import TodosPage from '../pages/Todos/Index';
 import LoginPage from "../pages/Login/Index";
 
+import PrivateRoute from "./PrivateRoute";
+
 const RoutesApp = () => {
 	return(
 			<Routes>
 				<Route path='/' element={ <HomePage /> } />
-				<Route path='/todos' element={ <TodosPage /> } />
-				<Route path='/login' element={ <LoginPage /> } />
+				<Route path='/todos' element={ <PrivateRoute> <TodosPage /> </PrivateRoute> } />
 
+				<Route path='/login' element={ <LoginPage /> } />
 				<Route path='*' element={ <HomePage /> } />
 			</Routes>
-	)
-}
+	);
+};
 
 export default RoutesApp;
